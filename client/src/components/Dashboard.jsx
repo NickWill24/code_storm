af6e0fc97a654fb12f6a57f89dceb678f060e7d1
Christina <christinapadilla.cp@gmail.com>
2021-04-08 16:20:53 -0500

    add components boilerplate

---
+++ client/src/components/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import IdeaList from './IdeaList'
+import Form from './Form'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Form />
+      <IdeaList />
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ client/src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+      <h3>Form</h3>
+      <form>
+        <input type="text" value="" placeholder="" />
+      </form>
+    </div>
+  )
+}
+
+export default Form
