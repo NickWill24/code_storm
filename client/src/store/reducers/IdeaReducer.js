0fae7fa7679b3115db45fb7c3d5f028a7d0cb5b9
nickwill24 <nick2428.nw@gmail.com>
2021-04-08 18:21:53 -0400

    IDEA STORE

--- client/src/store/reducers/IdeaReducer.js
+++ client/src/store/reducers/IdeaReducer.js
@@ -1,3 +1,37 @@
 // import types
 // define istate
 // define reduces
+
+import { GET_IDEAS, ADD_DESCRIPTION, ADD_TITLE, ADD_STACKS, CREATE_IDEA, REMOVE_IDEA, CREATE_IDEA, GET_IDEA, NUMBER_OF_LIKES } from "../types"
+
+const iState={
+    ideas:[],
+    title:'',
+    stacks: [],
+    description:'',
+    numberOfLikes: 0,
+    getIdea:[]
+}
+const IdeaReducers= (state= iState, action) => {
+    switch(action.type) {
+        case CREATE_IDEA: 
+            return{...state, title:'', description:''}
+        case REMOVE_IDEA:
+            return {...state}
+        case ADD_TITLE:
+            return{...state, title: action.payload}
+        case ADD_STACKS:
+            return{...state, stacks: action.payload}
+        case ADD_DESCRIPTION:
+            return{...state, description: action.payload}
+        case NUMBER_OF_LIKES:
+            return{...state,  numberOfLikes: action.payload}
+        case GET_IDEAS:
+            return{...state, ideas: action.payload}
+        case  GET_IDEA:
+            return{...state, idea: action.payload}
+            default:
+                return{...state }
+    }
+}
+export default IdeaReducers
--- client/src/store/types.js
+++ client/src/store/types.js
@@ -1,2 +1,10 @@
 /* Idea Types */
 export const GET_IDEAS = 'GET_IDEAS'
+export const CREATE_IDEA='CREATE_IDEA'
+export const REMOVE_IDEA='REMOVE_IDEA'
+export const ADD_TITLE='ADD_TITLE'
+export const ADD_DESCRIPTION="ADD_DESCRIPTION"
+export const ADD_STACKS="ADD_STACKS"
+export const GET_IDEAS= "GET_IDEAS"
+export const GET_IDEA= 'GET_IDEA'
+export const NUMBER_OF_LIKES= 'NUMBER_OF_LIKES'
