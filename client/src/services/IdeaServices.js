1c5ecfeb8c79ae50d845cabfc7c3c696e969698c
nickwill24 <nick2428.nw@gmail.com>
2021-04-08 21:08:17 -0400

    Idea Service and Action

--- client/src/services/IdeaServices.js
+++ client/src/services/IdeaServices.js
@@ -1,1 +1,46 @@
 import Client from './'
+
+
+export const GetIdeas = async () => {
+    try {
+        const res = await Client.get('/ideas')
+        return res.data
+    } catch (error) {
+        throw error
+    }
+}
+
+export const GetIdea = async (id) => {
+    try {
+        const res = await Client.get(`/ideas/${id}`)
+        return res 
+    } catch (error) {
+        throw error
+    }
+}
+
+export const CreateIdeas = async (formValues) => {
+    try {
+        const res = await Client.post('/ideas',formValues)
+        return res
+    } catch (error) {
+        throw error
+    }
+}
+
+export const RemoveIdeas = async (id) => {
+    try {
+        const res = await Client.delete(`/ideas/${id}`)
+        return res
+    } catch (error) {
+        throw error
+    }
+}
+// export const UpdateIdeas = async (id) => {
+//     try {
+//         const res = await Client.put(`/ideas/${id}`)
+//         return res
+//     } catch (error) {
+//         throw error
+//     }
+// }
