9bfd36cbd8d220dddd735742a8e85f740c520238
nickwill24 <nick2428.nw@gmail.com>
2021-05-05 11:46:45 -0400

    add baseurl

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,5 +1,8 @@
 import Axios from 'axios'
 
-const Client = Axios.create({ baseURL: 'http://localhost:5000' })
+const Client = Axios.create({  baseURL:
+    process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_API_URL
+    : 'http://localhost:5000'})
 
 export default Client
